e09e8f5a90e56c2a6aed70653c6f28e14bdf31b5
Maximo Espinal <maximoespinaldm@gmail.com>
2022-10-11 05:16:25 -0400

    fix breackpoint SE

--- src/componentes/AboutMe.js
+++ src/componentes/AboutMe.js
@@ -33,55 +33,54 @@ function AboutMe() {
                 <h2>Technologys</h2>
                 <Row>
                   {/* Icon */}
-                  <Col xs={6} md={4}>
+                  <Col xs={4} md={4}>
                     <FaHtml5 />
-                    <br />
                     <h6>HTML</h6>
                   </Col>
                   {/* Icon */}
-                  <Col xs={6} md={4}>
+                  <Col xs={4} md={4}>
                     <FaCss3Alt />
                     <br />
                     <h6>CSS</h6>
                   </Col>
                   {/* Icon */}
-                  <Col xs={6} md={4}>
+                  <Col xs={4} md={4}>
                     <FaBootstrap />
                     <br />
                     <h6>BootStrap</h6>
                   </Col>
                   {/* Icon */}
-                  <Col xs={6} md={4}>
+                  <Col xs={4} md={4}>
                     <FaJs />
                     <br />
                     <h6>JavaScript</h6>
                   </Col>
                   {/* Icon */}
-                  <Col xs={6} md={4}>
+                  <Col xs={4} md={4}>
                     <FaReact />
                     <br />
                     <h6>React</h6>
                   </Col>
                   {/* Icon */}
-                  <Col xs={6} md={4}>
+                  <Col xs={4} md={4}>
                     <FaPython />
                     <br />
                     <h6>Python</h6>
                   </Col>
                   {/* Icon */}
-                  <Col xs={6} md={4}>
+                  <Col xs={4} md={4}>
                     <FaFigma />
                     <br />
                     <h6>Figma</h6>
                   </Col>
                   {/* Icon */}
-                  <Col xs={6} md={4}>
+                  <Col xs={4} md={4}>
                     <SiMicrosoftsqlserver />
                     <br />
                     <h6>Microsoft SQL server</h6>
                   </Col>
                   {/* Icon */}
-                  <Col xs={6} md={4}>
+                  <Col xs={4} md={4}>
                     <GrMysql />
                     <br />
                     <h6>MySQL</h6>
